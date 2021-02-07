1ef993e0f72e609881b432b978b66012fd9c9ddf
winters0727 <winters0727@gmail.com>
2021-02-07 13:19:35 +0900

    finish second tutorial

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
+
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets import views
 
+# function based views
+# urlpatterns = [
+#     path('', views.snippet_list),
+#     path('<int:pk>/', views.snippet_detail),
+# ]
+
+# class based views
 urlpatterns = [
-    path('', views.snippet_list),
-    path('<int:pk>/', views.snippet_detail),
+    path('', views.SnippetList.as_view()),
+    path('<int:pk>/', views.SnippetDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
